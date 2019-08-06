177fe2ec1117b804c8546afe6528df39e1b21efe
Laura <laurajadefogarty@gmail.com>
2019-08-06 22:42:43 +0100

    initial Events styling

--- Components/Events.js
+++ Components/Events.js
@@ -1,19 +1,21 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 import moment from "moment";
 import styles from "./Styling/styling";
 
 const EventsScreen = props => (
   <View style={styles.eventsMain}>
     <Text style={styles.eventsTitle}>Events</Text>
-    {props.screenProps.events.map(({ id, time, description, user_id }) => (
-      <View key={id} style={styles.eventsSingle}>
-        <Text style={styles.eventsText}>{`Time: ${moment(time).format(
-          "DD-MM-YYYY HH:MM"
-        )}`}</Text>
-        <Text style={styles.eventsText}>{`Event: ${description}`}</Text>
-      </View>
-    ))}
+    <ScrollView>
+      {props.screenProps.events.map(({ id, time, description, user_id }) => (
+        <View key={id} style={styles.eventsSingle}>
+          <Text style={styles.eventsText}>{`Time: ${moment(time).format(
+            "DD-MM-YYYY HH:MM"
+          )}`}</Text>
+          <Text style={styles.eventsText}>{`Event: ${description}`}</Text>
+        </View>
+      ))}
+    </ScrollView>
   </View>
 );
 
