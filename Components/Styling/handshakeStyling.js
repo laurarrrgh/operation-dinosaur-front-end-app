821b47b70f407f3940b5f1f68abdf961816e0730
Laura <laurajadefogarty@gmail.com>
2019-08-07 22:54:29 +0100

    amends placeholder styling

--- Components/Styling/handshakeStyling.js
+++ Components/Styling/handshakeStyling.js
@@ -8,7 +8,13 @@ export default (styles = StyleSheet.create({
     padding: 50,
     backgroundColor: "#E8EDEE"
   },
-  handshakeTitle: { fontSize: 24, fontWeight: "bold", paddingBottom: 20 },
+  handshakeTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    paddingBottom: 20,
+    paddingTop: 20,
+    textAlign: "center"
+  },
   handshakeText: { fontSize: 18, margin: 10, textAlign: "center" },
   codeText: {
     fontWeight: "bold",
