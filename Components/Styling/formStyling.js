14ebbec2288427d2e928c3877810e3a6556aba17
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-08-08 12:55:14 +0100

    merge form styles

--- Components/Styling/formStyling.js
+++ Components/Styling/formStyling.js
@@ -1,28 +1,39 @@
 import React, { StyleSheet } from "react-native";
 
 export default (styles = StyleSheet.create({
-  addMedsMain: {
+  formMain: {
     paddingTop: 50,
     paddingLeft: 20,
     backgroundColor: "white"
   },
-  addMedsTextInput: {
-    height: 50,
-    borderColor: "#003087",
-    borderWidth: 1,
-    marginHorizontal: 20,
-    marginTop: 20,
-    borderRadius: 15,
-    borderWidth: 1.5,
+  formTitle: {
+    fontSize: 35,
+    fontWeight: "bold",
+    marginVertical: 10,
     textAlign: "center"
   },
-  addMedsButton: {
+  formTextInput: {
+    borderColor: "#003087",
+    borderRadius: 15,
+    borderWidth: 1.5,
+    height: 50,
+    marginHorizontal: 20,
+    paddingHorizontal: 2,
+    textAlign: "center"
+  },
+  formButton: {
     justifyContent: "center",
     textAlign: "center",
     marginHorizontal: 90,
     marginTop: 10,
-    fontSize: 50,
     fontWeight: "bold",
     backgroundColor: "#FAE100"
+  },
+  formText: {
+    padding: 10,
+    marginTop: 10,
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "bold"
   }
 }));
