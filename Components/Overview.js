6dd60ece3ea6864daa748f8907aba8001477b2a5
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-08-08 12:42:34 +0100

    remove additional views

--- Components/Overview.js
+++ Components/Overview.js
@@ -28,137 +28,127 @@ const OverviewScreen = props => {
       <HeaderBar />
       <ScrollView style={styles.graphScrollView}>
         <Text style={styles.graphMainTitle}>Overview</Text>
-        <View>
-          <Text style={styles.graphTitle}>Meds Taken</Text>
-          <PieChart
-            data={[
-              { name: "Taken", quantity: 3, color: "#005EB8" },
-              { name: "Missed", quantity: 1, color: "#FAE100" }
-            ]}
-            width={Dimensions.get("window").width}
-            height={200}
-            chartConfig={{
-              backgroundColor: "#005EB8",
-              decimalPlaces: 2,
-              color: () => `white`,
-              style: {
-                borderRadius: 16
+        <Text style={styles.graphTitle}>Meds Taken</Text>
+        <PieChart
+          data={[
+            { name: "Taken", quantity: 3, color: "#005EB8" },
+            { name: "Missed", quantity: 1, color: "#FAE100" }
+          ]}
+          width={Dimensions.get("window").width}
+          height={200}
+          chartConfig={{
+            backgroundColor: "#005EB8",
+            decimalPlaces: 2,
+            color: () => `white`,
+            style: {
+              borderRadius: 16
+            }
+          }}
+          accessor="quantity"
+          backgroundColor="transparent"
+          paddingLeft="15"
+          absolute
+        />
+        <Text style={styles.graphTitle}>Mood Results</Text>
+        <LineChart
+          data={{
+            labels: ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed"],
+            datasets: [
+              {
+                data: moodResults.slice(-7),
+                color: () => `#003087`,
+                strokeWidth: 2
               }
-            }}
-            accessor="quantity"
-            backgroundColor="transparent"
-            paddingLeft="15"
-            absolute
-          />
-        </View>
-        <View>
-          <Text style={styles.graphTitle}>Mood Results</Text>
-          <LineChart
-            data={{
-              labels: ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed"],
-              datasets: [
-                {
-                  data: moodResults.slice(-7),
-                  color: () => `#003087`,
-                  strokeWidth: 2
-                }
-              ]
-            }}
-            width={Dimensions.get("window").width}
-            height={220}
-            chartConfig={{
-              backgroundColor: "#E8EDEE",
-              backgroundGradientFrom: "#FAE100",
-              backgroundGradientTo: "#005EB8",
-              decimalPlaces: 2,
-              color: () => `white`,
-              style: {
-                borderRadius: 26
+            ]
+          }}
+          width={Dimensions.get("window").width}
+          height={220}
+          chartConfig={{
+            backgroundColor: "#E8EDEE",
+            backgroundGradientFrom: "#FAE100",
+            backgroundGradientTo: "#005EB8",
+            decimalPlaces: 2,
+            color: () => `white`,
+            style: {
+              borderRadius: 26
+            }
+          }}
+        />
+        <Text style={styles.graphTitle}>Slowness Results</Text>
+        <LineChart
+          data={{
+            labels: ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed"],
+            datasets: [
+              {
+                data: slownessResults.slice(-7),
+                color: () => `#003087`,
+                strokeWidth: 2
               }
-            }}
-          />
-        </View>
-        <View>
-          <Text style={styles.graphTitle}>Slowness Results</Text>
-          <LineChart
-            data={{
-              labels: ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed"],
-              datasets: [
-                {
-                  data: slownessResults.slice(-7),
-                  color: () => `#003087`,
-                  strokeWidth: 2
-                }
-              ]
-            }}
-            width={Dimensions.get("window").width}
-            height={220}
-            chartConfig={{
-              backgroundColor: "#E8EDEE",
-              backgroundGradientFrom: "#FAE100",
-              backgroundGradientTo: "#005EB8",
-              decimalPlaces: 2,
-              color: () => `white`,
-              style: {
-                borderRadius: 16
+            ]
+          }}
+          width={Dimensions.get("window").width}
+          height={220}
+          chartConfig={{
+            backgroundColor: "#E8EDEE",
+            backgroundGradientFrom: "#FAE100",
+            backgroundGradientTo: "#005EB8",
+            decimalPlaces: 2,
+            color: () => `white`,
+            style: {
+              borderRadius: 16
+            }
+          }}
+        />
+        <Text style={styles.graphTitle}>Stiffness Results</Text>
+        <LineChart
+          data={{
+            labels: ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed"],
+            datasets: [
+              {
+                data: stiffnessResults.slice(-7),
+                color: () => `#003087`,
+                strokeWidth: 2
               }
-            }}
-          />
-        </View>
-        <View>
-          <Text style={styles.graphTitle}>Stiffness Results</Text>
-          <LineChart
-            data={{
-              labels: ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed"],
-              datasets: [
-                {
-                  data: stiffnessResults.slice(-7),
-                  color: () => `#003087`,
-                  strokeWidth: 2
-                }
-              ]
-            }}
-            width={Dimensions.get("window").width}
-            height={220}
-            chartConfig={{
-              backgroundColor: "#E8EDEE",
-              backgroundGradientFrom: "#FAE100",
-              backgroundGradientTo: "#005EB8",
-              decimalPlaces: 2,
-              color: () => `white`,
-              style: {
-                borderRadius: 16
+            ]
+          }}
+          width={Dimensions.get("window").width}
+          height={220}
+          chartConfig={{
+            backgroundColor: "#E8EDEE",
+            backgroundGradientFrom: "#FAE100",
+            backgroundGradientTo: "#005EB8",
+            decimalPlaces: 2,
+            color: () => `white`,
+            style: {
+              borderRadius: 16
+            }
+          }}
+        />
+        <Text style={styles.graphTitle}>Tremor Results</Text>
+        <LineChart
+          data={{
+            labels: ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed"],
+            datasets: [
+              {
+                data: tremorResults.slice(-7),
+                color: () => `#003087`,
+                strokeWidth: 2
               }
-            }}
-          />
-        </View>
-        <View>
-          <Text style={styles.graphTitle}>Tremor Results</Text>
-          <LineChart
-            data={{
-              labels: ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed"],
-              datasets: [
-                {
-                  data: tremorResults.slice(-7),
-                  color: () => `#003087`,
-                  strokeWidth: 2
-                }
-              ]
-            }}
-            width={Dimensions.get("window").width}
-            height={220}
-            chartConfig={{
-              backgroundColor: "#E8EDEE",
-              backgroundGradientFrom: "#FAE100",
-              backgroundGradientTo: "#005EB8",
-              decimalPlaces: 2,
-              color: () => `white`,
-              style: {
-                borderRadius: 16
-              }
-            }}
-          />
-        </View>
+            ]
+          }}
+          width={Dimensions.get("window").width}
+          height={220}
+          chartConfig={{
+            backgroundColor: "#E8EDEE",
+            backgroundGradientFrom: "#FAE100",
+            backgroundGradientTo: "#005EB8",
+            decimalPlaces: 2,
+            color: () => `white`,
+            style: {
+              borderRadius: 16
+            }
+          }}
+        />
       </ScrollView>
     </View>
   );
